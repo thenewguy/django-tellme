9b6d2d53418bad6e9ae79f06a70ce347849b3f0c
Gordon Pendleton <wgordonw1@gmail.com>
2021-02-09 08:18:03 -0500

    used a list for the asset test

--- tellme/tests/test_assets.py
+++ tellme/tests/test_assets.py
@@ -2,14 +2,17 @@ from django.contrib.staticfiles import finders
 from django.test import TestCase
 
 
-MISSING_ASSET = 'Frontend asset missing. Did you build it?'
+MISSING_ASSET = 'Frontend asset "%s" missing. Did you build it?'
+
+ASSETS = [
+    'tellme/feedback/dist/feedback.js',
+    'tellme/feedback/dist/feedback.css',
+]
 
 
 class AssetTests(TestCase):
-    def test_finders_feedbackjs(self):
-        result = finders.find('tellme/feedback/dist/feedback.js')
-        self.assertTrue(result, MISSING_ASSET)
-    
-    def test_finders_feedbackcss(self):
-        result = finders.find('tellme/feedback/dist/feedback.css')
-        self.assertTrue(result, MISSING_ASSET)
+    def test_assets_exist(self):
+        for asset in ASSETS:
+            with self.subTest(asset=asset)
+                result = finders.find(asset)
+                self.assertTrue(result, MISSING_ASSET % asset)
